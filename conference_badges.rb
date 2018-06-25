211aa5a5ac5a2214452fb2799ff640908e07a55a
Davidlago104 <davidlago104@gmail.com>
2018-06-25 04:11:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,11 +4,5 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees) 
-  counter = 1 
   
-  while counter <= 8
-  puts "Hello, my name is #{name}."
-  break if counter == 7
-  counter = counter + 1 
-end
 end
